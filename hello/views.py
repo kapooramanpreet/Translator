94a49f05918e6fe7519d4e247a4119017dab28df
amanpreet kapoor <kapoorise@gmail.com>
2018-02-03 13:47:29 -0500

    ss

--- hello/views.py
+++ hello/views.py
@@ -22,7 +22,10 @@ def db(request):
 
 def x(request):
     if request.method=='POST':
-        return
+        context= {
+            'name':'nice
+        }
+        return render(request, 'x.html', context)
     else:
         subscriptionKey = '0c659fa4493f47c6b72473447b41fd4d'
         host = 'api.microsofttranslator.com'
