47f50491430153c2d8a9bf2c1f1fac0ed5794574
amanpreet kapoor <kapoorise@gmail.com>
2018-02-03 17:22:05 -0500

    ss

--- hello/views.py
+++ hello/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import http.client, urllib.parse
+import unicodedata
 
 from .models import Greeting
 
-l=[]
+l1=[]
+l2=[]
 # Create your views here.
 def index(request):
     # return HttpResponse('Hello from Python!')
@@ -12,40 +14,90 @@ def index(request):
 
 
 def db(request):
-
     greeting = Greeting()
     greeting.save()
-
     greetings = Greeting.objects.all()
-
     return render(request, 'db.html', {'greetings': greetings})
 
 
 def x(request):
     if request.method=='POST':
         title= request.POST['title']
-        box2= request.POST.get('box2', True)
-        subscriptionKey = '0c659fa4493f47c6b72473447b41fd4d'
-        host = 'api.microsofttranslator.com'
-        path = '/V2/Http.svc/Translate'
-        target = 'fr-fr'
-        text = title
-        params = '?to=' + target + '&text=' + urllib.parse.quote (text)
-        def get_suggestions():
-            headers = {'Ocp-Apim-Subscription-Key': subscriptionKey}
-            conn = http.client.HTTPSConnection(host)
-            conn.request ("GET", path + params, None, headers)
-            response = conn.getresponse ()
-            return response.read ()
-        result = get_suggestions ()
-        l.append(result)
-        context= {
-            'name':l
-        }
-        return render(request, 'x.html', context)
+        title2= request.POST['title2']
+        if len(title)==0 and len(title2)==0:
+            return render(request, 'x.html')
+        if len(title)>0:
+            l1.append(title)
+            subscriptionKey = '0c659fa4493f47c6b72473447b41fd4d'
+            host = 'api.microsofttranslator.com'
+            path = '/V2/Http.svc/Translate'
+            #Hindi
+            target = 'hi'
+            text = title
+            params = '?to=' + target + '&text=' + urllib.parse.quote (text)
+            def get_suggestions():
+                headers = {'Ocp-Apim-Subscription-Key': subscriptionKey}
+                conn = http.client.HTTPSConnection(host)
+                conn.request ("GET", path + params, None, headers)
+                response = conn.getresponse ()
+                return response.read ()
+            result = get_suggestions ()
+            result = result.decode("utf-8", "strict")
+            result=str(result)
+            if result.index('>')>0:
+                result=result[result.index('>')+1:]
+            if result.index('<')>0:
+                result=result[0:result.index('<')]
+            l2.append(result)
+            qq=""
+            for i in l2:
+                qq+=i+"\n"
+            cc=""
+            for i in l1:
+                cc+=i+"\n"
+            context= {
+                'text1':cc,
+                'text2':qq
+            }
+            return render(request, 'x.html', context)
+        else:
+            l2.append(title2)
+            subscriptionKey = '0c659fa4493f47c6b72473447b41fd4d'
+            host = 'api.microsofttranslator.com'
+            path = '/V2/Http.svc/Translate'
+            #Hindi
+            target = 'en'
+            text = title2
+            params = '?to=' + target + '&text=' + urllib.parse.quote (text)
+            def get_suggestions():
+                headers = {'Ocp-Apim-Subscription-Key': subscriptionKey}
+                conn = http.client.HTTPSConnection(host)
+                conn.request ("GET", path + params, None, headers)
+                response = conn.getresponse ()
+                return response.read ()
+            result = get_suggestions ()
+            result = result.decode("utf-8", "strict")
+            result=str(result)
+            if result.index('>')>0:
+                result=result[result.index('>')+1:]
+            if result.index('<')>0:
+                result=result[0:result.index('<')]
+            l1.append(result)
+            qq=""
+            for i in l2:
+                qq+=i+"\n"
+            cc=""
+            for i in l1:
+                cc+=i+"\n"
+            context= {
+                'text1':cc,
+                'text2':qq
+            }
+            return render(request, 'x.html', context)
     else:
         context= {
-            'name':'ok'
+            'text1':'',
+            'text2':''
         }
         #print (result.decode("utf-8"))
         return render(request, 'x.html', context)
